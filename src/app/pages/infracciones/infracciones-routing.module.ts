947c2cd65c7a63f4b6a60b3540469078b4d832e3
Alex Aguilar <aguilarpenaalex66@gmail.com>
2020-11-07 10:28:38 -0400

    cambios

--- src/app/pages/infracciones/infracciones-routing.module.ts
+++ src/app/pages/infracciones/infracciones-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { InfiniteListComponent } from '../layout/infinite-list/infinite-list.component';
+import { InfraccionesComponent } from './componentes/infracciones/infracciones.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: InfiniteListComponent,
+    component: InfraccionesComponent,
   },
 ];
 
