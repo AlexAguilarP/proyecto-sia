e602f7b0d6f41d90775bd7c55b468b7da066ccce
bpf311 <bpf311@gmail.com>
2020-12-17 15:00:54 -0400

    Reportes

--- src/app/pages/estadisticas/componentes/estadisticas/estadisticas.component.ts
+++ src/app/pages/estadisticas/componentes/estadisticas/estadisticas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
 
 @Component({
   selector: 'ngx-estadisticas',
@@ -11,8 +12,12 @@ export class EstadisticasComponent implements OnInit {
 
   constructor(private firestore: AngularFirestore, private db: AngularFireDatabase) { }
   myArray: any = [];
+  arrayInf: any = [];
+  fecha: any;
   objeto: any;
   multi: any;
+  datos: any;
+  contador: any = 0;
 
   ngOnInit(): void {
     this.db.list('conteo_autos').valueChanges().subscribe(data=>{
@@ -38,6 +43,48 @@ export class EstadisticasComponent implements OnInit {
         }
       ];
     });
+    this.firestore.collection('infracciones').valueChanges().subscribe(data => {
+      let con = 1
+      console.log(data);
+
+
+      /*this.fecha = data[0]['date']
+      for (let index = 1; index < data.length; index++) {
+
+        if (this.fecha == data[index]['date']) {
+          con = con + 1
+        }else{
+
+          this.arrayFechas.push(con)
+          con = 1
+        }
+        this.fecha = data[index]['date']
+
+      }
+      this.arrayFechas.push(con)
+
+      console.log(this.arrayFechas);*/
+
+      for (let index = 0; index < data.length ; index++) {
+        const fecha = new Date(data[index]['date']);
+
+        const element = {
+          name: fecha.toLocaleDateString('Es-es', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}) + ' ' + fecha.toLocaleTimeString(),
+          value: data.length
+        }
+        this.arrayInf.push(element);
+      }
+
+      console.log(this.arrayInf);
+      this.datos = [
+        {
+          name: "Cantidad de infracciones",
+          series: this.arrayInf
+        }
+      ];
+
+    });
+
   }
 
 
